a8896ea19a0cfa100a1b1e4ae83f64ba6ad37b0a
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-05 16:09:09 +0600

    added counter js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// ============ jQuery all operations =============
+$(function(){
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1500,
+});
+
+
+  
+})
+// ============ jQuery all operations =============
