ee80be96c8dec2487d1a3954ce1ec4876a7d929a
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-05 19:12:28 +0600

    collapse responsive nav design complete

--- js/script.js
+++ js/script.js
@@ -15,26 +15,101 @@ $(function(){
     prevArrow:'.games__sliderArrowLeft',
     nextArrow:'.games__sliderArrowRight'
   });
+
   $('.streaming__videoWrapper').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2500,
     arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
+
   $('.sale__itemWrapper').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
     arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
+
   $('.accessories__itemWrapper').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
   // ===== slick slider js =====
 
@@ -42,3 +117,20 @@ $(function(){
   
 })
 // ============ jQuery all operations =============
+
+// ========== script js operation ===========
+// ===== navbar js =====
+// all selector 
+const navMenu = document.querySelector('.nav__menu__wrapper')
+const collapseClose = document.querySelector('.collapse__menuIcon i')
+// all selector 
+
+// menu hide show 
+
+collapseClose.addEventListener('click',(e)=>{
+  navMenu.classList.toggle('nav__active')
+  collapseClose.classList.toggle('fa-xmark')
+})
+// ===== navbar js =====
+
+// ========== script js operation ===========
