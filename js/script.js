f68ee3c9a4fc9824cac87010709647dd4f1b4590
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-06 19:43:43 +0600

    added sale section responsive

--- js/script.js
+++ js/script.js
@@ -56,24 +56,24 @@ $(function(){
     arrows:false,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1124,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1
         }
       }
